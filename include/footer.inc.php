35e73b1973e775021847b890c493f10d2f1dd7b5
vaylonn <114398794+vaylonn@users.noreply.github.com>
2023-12-12 12:32:01 +0100

    Update footer.inc.php

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,16 +1,41 @@
-    </main>
-    <footer>
+</main>
+<footer>
+    <link rel="stylesheet" href="css-jour.css">
     <div class="footer-content">
         <div class="footer-section">
-            <h3>Contact</h3>
-            <p>Email: contact@votreboite.com</p>
-            <p>Téléphone: +33 123 456 789</p>
+            <h3>TEST</h3>
+            <a href="https://github.com/MansourInes/night-owls-" target="_blank" rel="noopener noreferrer">Github
+            </a>
+            <a href="https://www.isep.fr/" target="_blank" rel="noopener noreferrer">ISEP</a>
         </div>
-
+        <div class="footer-section">
+            <h3>CONTACTS</h3>
+            <a href="https://github.com/MansourInes/night-owls-" target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="https://www.isep.fr/" target="_blank" rel="noopener noreferrer">ISEP</a>
+        </div>
+        <div class="footer-section">
+            <h3>A PROPOS</h3>
+            <a href="https://github.com/MansourInes/night-owls-" target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="https://www.isep.fr/" target="_blank" rel="noopener noreferrer">ISEP</a>
+        </div>
+        <div class="footer-section">
+            <h3>SUPPORT</h3>
+            <a href="https://github.com/MansourInes/night-owls-" target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="https://www.isep.fr/" target="_blank" rel="noopener noreferrer">ISEP</a>
+        </div>
+        <div class="footer-section">
+            <h3>FORUM</h3>
+            <a href="https://github.com/MansourInes/night-owls-" target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="https://www.isep.fr/" target="_blank" rel="noopener noreferrer">ISEP</a>
+        </div>
+        <div class="footer-section">
+            <h3>RESEAU</h3>
+            <a href="https://github.com/MansourInes/night-owls-" target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="https://www.isep.fr/" target="_blank" rel="noopener noreferrer">ISEP</a>
+        </div>
     </div>
-
     <div class="footer-bottom">
-        <p>&copy; <?=date('Y')?> Night Owls. Tous droits réservés. | Site web créé par MANSOUR Ines</p>
+        <p>&copy; <?=date('Y')?> Night Owls. Tous droits réservés. | Site web créé par le groupe InnovTech</p>
     </div>
 </footer>
 </body>
